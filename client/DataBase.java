006451350b12f0715032ff151d2dedbf70ae6828
Daria Zhdanova <94113384+daria-dsg@users.noreply.github.com>
2024-03-20 07:24:05 +0100

    add set method to add string to the database

--- client/DataBase.java
+++ client/DataBase.java
@@ -5,6 +5,17 @@ import java.util.Collections;
 import java.util.List;
 
 public class DataBase {
-    List<String> data = new ArrayList<>(Collections.nCopies(100, ""));
+    List<String> listOfData = new ArrayList<>(Collections.nCopies(100, ""));
+
+    void set (int num, String input) {
+        int index = num - 1;
+
+        if (index >= 0 && index <= 99) {
+            listOfData.set(index, input);
+            System.out.println("OK");
+        } else {
+            System.out.println("ERROR");
+        }
+    }
 
 }
