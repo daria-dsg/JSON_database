68d4d3ae8317f6cee03ff8cf1c990205b7e62b65
Daria Zhdanova <94113384+daria-dsg@users.noreply.github.com>
2024-03-20 08:08:36 +0100

    put database in server package

--- server/Main.java
+++ server/Main.java
@@ -1,8 +1,20 @@
 package server;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Scanner scanner = new Scanner(System.in);
+        DataBase dataBase = new DataBase();
+
+        String command = scanner.next();
+        int num = scanner.nextInt();
+        String input = scanner.next();
+
+        switch (command) {
+            case "set" : dataBase.set(num, input);
+        }
+
     }
 }
