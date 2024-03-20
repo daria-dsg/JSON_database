94470eb43c4f2509370452667f6c9609f6c868c9
Daria Zhdanova <94113384+daria-dsg@users.noreply.github.com>
2024-03-20 07:16:48 +0100

    add database java class with array which stores strings

---
+++ client/DataBase.java
@@ -0,0 +1,10 @@
+package client;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DataBase {
+    List<String> data = new ArrayList<>(Collections.nCopies(100, ""));
+
+}
