ff8d17a3a93fc4a870dd473757d1acf61213d6d8
Daria Zhdanova <94113384+daria-dsg@users.noreply.github.com>
2024-04-02 19:58:54 +0200

    add client side of communication

--- client/Main.java
+++ client/Main.java
@@ -1,8 +1,28 @@
 package client;
 
-public class Main {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        try (
+             Socket socket = new Socket("localhost", 9999);
+             DataInputStream input = new DataInputStream(socket.getInputStream());
+             DataOutputStream output = new DataOutputStream(socket.getOutputStream())
+        ) {
+
+            System.out.println("Client started!");
+            String msg = "Give me a record # 12";
+            output.writeUTF(msg); //sent message to server
+            System.out.println("Sent: " + msg);
+
+            String reply = input.readUTF();
+            System.out.println("Received: " + reply);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
