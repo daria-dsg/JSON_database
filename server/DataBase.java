5391dae589c25767917920e5f498729c4f19510d
Daria Zhdanova <94113384+daria-dsg@users.noreply.github.com>
2024-04-03 14:55:57 +0200

    enable methods to return string to be proceed by server

--- server/DataBase.java
+++ server/DataBase.java
@@ -1,78 +1,55 @@
 package server;
 
 import java.util.List;
-import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Objects;
 
 public class DataBase {
     private final List<String> listOfData = new ArrayList<>(Collections.nCopies(999, ""));
 
-    protected void run(String input) {
-
-        Scanner inputScanner = new Scanner(input);
-
-        // Read the first and the second token
-        String command = inputScanner.next();
-        int cellNumber = inputScanner.nextInt();
-
-        switch (Objects.requireNonNull(command)) {
-            case "set" :
-                String text = inputScanner.nextLine().trim();
-                set(cellNumber, text);
-                break;
-            case "get" :
-                get(cellNumber);
-                break;
-            case "delete":
-                delete(cellNumber);
-                break;
-        }
-
-        inputScanner.close();
-    }
-
-    private void set (int cell, String input) {
+    protected  String set (int cell, String input) {
         int index = cell - 1;
 
         if (isIndexValid(index)) {
             listOfData.set(index, input);
-            System.out.println("OK");
+            return "OK";
         } else {
-            System.out.println("ERROR");
+            return "ERROR";
         }
     }
 
-    private void get (int cell) {
+    protected String get (int cell) {
         int index = cell - 1;
 
         if (isIndexValid(index)) {
             String data = listOfData.get(index);
             if ("".equals(data)) {
-                System.out.println("ERROR");
+                return "ERROR";
             } else {
-                System.out.println(data);
+                return data;
             }
         } else {
-            System.out.println("ERROR");
+            return "ERROR";
         }
     }
 
-    private void delete (int cell) {
+    protected String delete (int cell) {
         int index = cell - 1;
 
         if (isIndexValid(index)) {
             String data = listOfData.get(index);
             if (!"".equals(data)) {
                 listOfData.remove(index);
+                return "OK";
             }
         } else {
-            System.out.println("ERROR");
+            return "ERROR";
         }
+
+        return "ERROR";
     }
 
     private boolean isIndexValid(int index) {
-        return index >= 0 && index < 99;
+        return index >= 0 && index < 9999;
     }
 }
