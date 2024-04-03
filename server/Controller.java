230989d52eec0a79f91be996c9bcb02248dee4b8
Daria Zhdanova <94113384+daria-dsg@users.noreply.github.com>
2024-04-03 15:56:37 +0200

    create invoker to carry out the request

---
+++ server/Controller.java
@@ -0,0 +1,17 @@
+package server;
+
+import server.command.Command;
+
+public class Controller {
+    private Command command;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void executeCommand(Object... args) {
+        command.execute(args);
+    }
+
+
+}
