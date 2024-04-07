172ef19ad329ae82d9b03dfefdac398bc99005e0
Daria Zhdanova <94113384+daria-dsg@users.noreply.github.com>
2024-04-07 13:07:35 +0200

    Make server continuously listens for client

--- server/Main.java
+++ server/Main.java
@@ -2,6 +2,7 @@ package server;
 
 import server.command.*;
 
+import javax.xml.crypto.Data;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -10,50 +11,46 @@ import java.net.Socket;
 
 public class Main {
 
-
     public static void main(String[] args) {
-        try(
-            ServerSocket serverSocket = new ServerSocket(9999)
-        ) {
+        try (ServerSocket serverSocket = new ServerSocket(9999)) {
             System.out.println("Server started!");
-            try (
-                Socket socket = serverSocket.accept();
-                DataInputStream input = new DataInputStream(socket.getInputStream());
-                DataOutputStream output = new DataOutputStream(socket.getOutputStream())
-            ) {
-                final DataBase db = new DataBase();
-                Controller controller = new Controller();
+            DataBase db = DataBase.getInstance();
 
-                String reply = input.readUTF(); //read msg from client
+            while(true) {
+                try (Socket socket = serverSocket.accept();
+                     DataInputStream input = new DataInputStream(socket.getInputStream());
+                     DataOutputStream output = new DataOutputStream(socket.getOutputStream())) {
 
-                String[] parts = reply.split("\\s+");
+                     Controller controller = new Controller();
 
-                // Extract the command and arguments
-                String command = parts[0];
-                int number = Integer.parseInt(parts[1]);
-                String message = null;
+                     String reply = input.readUTF(); //read msg from client
+                     String[] parts = reply.split("\\s+");
 
-                if ("set".equals(command)) {
-                    message = parts[2];
+                     // Extract the command and arguments
+                     String command = parts[0];
+                     int number = parts.length > 1 ? Integer.parseInt(parts[1]) : 0;
+                     String message = parts.length > 2 ? parts[2] : null;
+                     String msg = null;
+
+                     switch(command) {
+                        case "get":
+                            controller.setCommand(new GetCell(db));
+                            msg = controller.executeCommand(number);
+                            break;
+                        case "set":
+                            controller.setCommand(new SetCell(db));
+                            msg = controller.executeCommand(number, message);
+                            break;
+                        case "delete":
+                            controller.setCommand(new DeleteCell(db));
+                            msg = controller.executeCommand(number);
+                            break;
+                    }
+                    output.writeUTF(msg);
                 }
-
-                String msg = null;
-
-                switch(command) {
-                    case "get":
-                        controller.setCommand(new GetCell(db));
-                        msg = controller.executeCommand(number);
-                        break;
-                    case "set":
-                        controller.setCommand(new SetCell(db));
-                        msg = controller.executeCommand(number, message);
-                        break;
-                    case "delete":
-                        controller.setCommand(new DeleteCell(db));
-                        msg = controller.executeCommand(number);
-                        break;
+                catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
-                output.writeUTF(msg);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
