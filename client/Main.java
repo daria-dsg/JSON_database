506cb7f96f32fc825622247012dbf82bb5fe8527
Daria Zhdanova <94113384+daria-dsg@users.noreply.github.com>
2024-05-20 22:29:25 +0200

    serialize CommandLineArgument to JSON

--- client/Main.java
+++ client/Main.java
@@ -2,6 +2,7 @@ package client;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import com.google.gson.Gson;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -11,50 +12,34 @@ import java.net.Socket;
 public class Main {
     public static void main(String[] args) {
 
-        //define class to hold arguments values
-        class CommandLineArgs {
-
-        }
-        CommandLineArgs cli  = new CommandLineArgs();
+        CommandLineArgs cla  = new CommandLineArgs();
 
         // Parse the command-line arguments
         JCommander.newBuilder()
-                .addObject(arguments)
+                .addObject(cla)
                 .build()
                 .parse(args);
 
-        //build message to sent to server
-        String msg = buildMsg(arguments.type, arguments.idOfCell, arguments.message);
+        String request = new Gson().toJson(cla);
+        System.out.println(request);
 
-        try (
-             Socket socket = new Socket("localhost", 9999);
-             DataInputStream input = new DataInputStream(socket.getInputStream());
-             DataOutputStream output = new DataOutputStream(socket.getOutputStream())
-        ) {
-            System.out.println("Client started!");
-            output.writeUTF(msg); //sent message to server
-            System.out.println("Sent: " + msg);
-
-            String reply = input.readUTF();
-            System.out.println("Received: " + reply);
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+//        try (
+//             Socket socket = new Socket("localhost", 9999);
+//             DataInputStream input = new DataInputStream(socket.getInputStream());
+//             DataOutputStream output = new DataOutputStream(socket.getOutputStream())
+//        ) {
+//            System.out.println("Client started!");
+//            output.writeUTF(msg); //sent message to server
+//            System.out.println("Sent: " + msg);
+//
+//            String reply = input.readUTF();
+//            System.out.println("Received: " + reply);
+//
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 
-    private static String buildMsg(String type, int index, String message) {
-        String msg = type;
-
-        if (index > 0) {
-            msg += " " + index;
-        }
-        if (message != null) {
-            msg += " " + message;
-        }
-
-        return msg;
-    }
 }
 
 
