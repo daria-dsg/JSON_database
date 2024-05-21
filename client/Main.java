7a083c3a6de0ba65f7809eb0976586fe29a5f8ab
Daria Zhdanova <94113384+daria-dsg@users.noreply.github.com>
2024-05-21 22:04:12 +0200

    Refactor CLA class

--- client/Main.java
+++ client/Main.java
@@ -2,6 +2,7 @@ package client;
 
 import com.beust.jcommander.JCommander;
 import com.google.gson.Gson;
+import server.model.Request;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -11,7 +12,7 @@ import java.net.Socket;
 public class Main {
     public static void main(String[] args) {
 
-        CommandLineArgs cla  = new CommandLineArgs();
+        Request cla  = new Request();
 
         // Parse the command-line arguments
         JCommander.newBuilder()
@@ -19,20 +20,23 @@ public class Main {
                 .build()
                 .parse(args);
 
-        String request = new Gson().toJson(cla);
+        System.out.println(cla);
 
-        try (
-             Socket socket = new Socket("localhost", 9999);
-             DataInputStream input = new DataInputStream(socket.getInputStream());
-             DataOutputStream output = new DataOutputStream(socket.getOutputStream())
-        ) {
-            System.out.println("Client started!");
-            output.writeUTF(request); //sent request to server
-            System.out.println("Sent: " + request);
-            System.out.println("Received: " + input.readUTF());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        String request = new Gson().toJson(cla);
+        System.out.println(request);
+
+//        try (
+//             Socket socket = new Socket("localhost", 9999);
+//             DataInputStream input = new DataInputStream(socket.getInputStream());
+//             DataOutputStream output = new DataOutputStream(socket.getOutputStream())
+//        ) {
+//            System.out.println("Client started!");
+//            output.writeUTF(request); //sent request to server
+//            System.out.println("Sent: " + request);
+//            System.out.println("Received: " + input.readUTF());
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 }
 
