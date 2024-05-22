1c33500d4f88db9b444c46594cd9888e8d45bca6
Daria Zhdanova <94113384+daria-dsg@users.noreply.github.com>
2024-05-22 18:38:17 +0200

    Implement setters method in Response class

--- server/model/Response.java
+++ server/model/Response.java
@@ -4,4 +4,16 @@ public class Response {
     private String response;
     private String reason;
     private String value;
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
