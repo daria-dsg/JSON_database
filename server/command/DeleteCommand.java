fa8b62df8a22cdc45ba027e4a0d78dc3009d6246
Daria Zhdanova <94113384+daria-dsg@users.noreply.github.com>
2024-05-22 19:04:20 +0200

    Unable result returning of delete command

--- server/command/DeleteCommand.java
+++ server/command/DeleteCommand.java
@@ -5,18 +5,13 @@ import server.database.DataBase;
 public class DeleteCommand implements Command {
 
     private String key;
-    private String result;
 
     public DeleteCommand (String key) {
         this.key = key;
     }
 
-    public String getResult() {
-        return result;
-    }
-
     @Override
     public void execute() {
-        result = DataBase.getInstance().delete(key);
+        DataBase.getInstance().delete(key);
     }
 }
